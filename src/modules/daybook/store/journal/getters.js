18050088abd62ea378ef298ad79fcb2db4fc1502
Erick Guzman <guzmi.dev@gmail.com>
2022-04-19 03:12:48 -0600

    :zap: mapGetters

--- src/modules/daybook/store/journal/getters.js
+++ src/modules/daybook/store/journal/getters.js
@@ -1,5 +1,13 @@
 // export const myGetter = (state) =>{
 // return state.algo
 // }
-export const getEntriesByTerm = (/*state*/) => {};
+export const getEntriesByTerm =
+  (state) =>
+  (term = "") => {
+    if (term.length === 0) return state.entries;
+
+    return state.entries.filter((entry) =>
+      entry.text.toLowerCase().includes(term.toLocaleLowerCase())
+    );
+  };
 export const getEntriesById = (/*state*/) => {};
