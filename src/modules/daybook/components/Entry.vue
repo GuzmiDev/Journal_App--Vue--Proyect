99a653c6a06cc4d6cd11829998506aaabb697060
Erick Guzman <guzmi.dev@gmail.com>
2022-04-19 03:24:45 -0600

    :art: EntryComponent

--- src/modules/daybook/components/Entry.vue
+++ src/modules/daybook/components/Entry.vue
@@ -1,22 +1,52 @@
 <template>
   <div
     class="entry-container mb-3 pointer p-2"
-    @click="$router.push({ name: 'entry', params: { id: 10 } })"
+    @click="$router.push({ name: 'entry', params: { id: entry.id } })"
   >
     <div class="entry-title d-flex">
-      <span class="text-success fs-5 fw-bold">15</span>
-      <span class="mx-1 fs-5">Julio</span>
-      <span class="mx-2 fw-light">2021, jueves</span>
+      <span class="text-success fs-5 fw-bold">{{ entryDay }}</span>
+      <span class="mx-1 fs-5">{{ entryMoth }}</span>
+      <span class="mx-2 fw-light">{{ entryYear }}</span>
     </div>
     <div class="entry-description">
-      Consectetur non aliqua cupidatat cupidatat adipisicing magna non sint
-      nostrud quis nostrud velit.
+      {{ shortText }}
     </div>
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  props: {
+    entry: {
+      type: Object,
+      required: true,
+    },
+  },
+  computed: {
+    shortText() {
+      return this.entry.text.length > 130
+        ? this.entry.text.substring(0, 130) + "..."
+        : this.entry.text;
+    },
+    entryDay() {
+      const day = new Date(this.entry.date).getDate();
+      return day;
+    },
+    entryMoth() {
+      const moth = new Date(this.entry.date).toLocaleString("default", {
+        month: "long",
+      });
+      return moth;
+    },
+    entryYear() {
+      const year = new Date(this.entry.date).toLocaleString("default", {
+        year: "numeric",
+        weekday: "long",
+      });
+      return year;
+    },
+  },
+};
 </script>
 
 <style lang="scss" scoped>
